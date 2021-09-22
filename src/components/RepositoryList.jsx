a853084c26b2fcc1531e69a47ee1ceae6e1e768f
Vyc <vyctoriak@gmail.com>
2021-09-22 20:29:09 -0300

    configurando sass

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>
+                Lista de repositórios
+            </h1>
+
+            <ul>
+                <li>
+                    <strong>
+
+                    </strong>
+                </li>
+            </ul>
+        </section>
+    )
+}
