ac2711bebab7a40577cfe6057db4fcfa6b4fd104
Vyctoria Karina <vyctoriak@gmail.com>
2023-09-12 21:53:42 +0200

    Improving snapshot tests with jest-style-components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -14,4 +14,12 @@ describe('<Main />', () => {
     // generate snapshot
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly', () => {
+    // render the component
+    const { container } = render(<Main />)
+
+    // verify if the background-color is correct
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
