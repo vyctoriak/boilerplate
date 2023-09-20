fd9367fa594e405b89f593e377c838265353ff05
Vyctoria Karina <vyctoriak@gmail.com>
2023-09-20 15:47:01 +0200

    update main component and add images

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -10,3 +10,10 @@ export default {
 } as Meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'title basic',
+    description: 'description basic'
+  }
+}
